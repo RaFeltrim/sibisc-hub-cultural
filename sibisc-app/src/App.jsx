2e4629315831bc9aaa4a20e845d22ba4cacb16aa
dudpaz <dudupaz2006@gmail.com>
2026-03-23 21:18:22 -0300

    fix: atualiza rotas para incluir Inicio, Noticias, Eventos e Acervo

--- sibisc-app/src/App.jsx
+++ sibisc-app/src/App.jsx
@@ -5,8 +5,10 @@ function App() {
     <Router>
       <div>
         <h1>SIBiSC - MVP Inicial</h1>
+        {/* Futuro componente de Header entrará aqui */}
         <Routes>
-          <Route path="/" element={<h2>Página Inicial (Notícias em breve)</h2>} />
+          <Route path="/" element={<h2>Página Início</h2>} />
+          <Route path="/noticias" element={<h2>Página de Notícias</h2>} />
           <Route path="/eventos" element={<h2>Página de Eventos</h2>} />
           <Route path="/acervo" element={<h2>Página de Acervo</h2>} />
         </Routes>
